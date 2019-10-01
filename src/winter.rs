a2ad4533ce7ee9a86c999022ea3e6b232c60e3bb
Arne Dußin <arne.dussin@live.de>
2019-10-01 18:01:30 +0200

    Move player initialisation to the player module

--- src/winter.rs
+++ src/winter.rs
@@ -1,10 +1,9 @@
 use amethyst::{
     prelude::*,
-    assets::{AssetStorage, Handle, Loader},
     core::{Named, Parent, Transform},
     ecs::{Entity, ReadStorage, Join},
     input::{is_close_requested, is_key_down},
-    renderer::{Camera, ImageFormat, SpriteRender, SpriteSheet, SpriteSheetFormat, Texture, Transparent},
+    renderer::Camera,
     window::ScreenDimensions,
     winit
 };
@@ -18,9 +17,8 @@ impl SimpleState for Winter {
         let world = data.world;
         world.register::<Named>();
 
-        let player_sprite_sheet_handle = load_sprite_sheet(world, "player_sheet.png");
 
-        let player = init_player(world, &player_sprite_sheet_handle);
+        let player = Player::init(world);
         let _camera = init_camera(world, player);
     }
 
@@ -46,24 +44,6 @@ impl SimpleState for Winter {
     }
 }
 
-fn init_player(world: &mut World, sprite_sheet: &Handle<SpriteSheet>) -> Entity {
-    let mut transform = Transform::default();
-    transform.set_translation_xyz(0., 0., -3.);
-
-    let sprite = SpriteRender {
-        sprite_sheet: sprite_sheet.clone(),
-        sprite_number: 1
-    };
-
-    world.create_entity()
-        .with(transform)
-        .with(sprite)
-        .with(Player::new())
-        .with(Transparent)
-        .named("player")
-        .build()
-}
-
 fn init_camera(world: &mut World, to_follow: Entity) -> Entity {
     let (width, height) = {
         let dim = world.read_resource::<ScreenDimensions>();
@@ -79,19 +59,4 @@ fn init_camera(world: &mut World, to_follow: Entity) -> Entity {
         .with(Camera::standard_2d(width, height))
         .named("camera")
         .build()
-}
-
-fn load_sprite_sheet(world: &mut World, image_path: &str) -> Handle<SpriteSheet> {
-    let loader = world.read_resource::<Loader>();
-
-    let texture_handle = {
-        let texture_storage = world.read_resource::<AssetStorage<Texture>>();
-        loader.load(image_path, ImageFormat::default(), (), &texture_storage)
-    };
-
-    let sprite_sheet_store = world.read_resource::<AssetStorage<SpriteSheet>>();
-    let mut ron_path = String::from(image_path.split_at(image_path.rfind('.').expect("Image has no file type")).0);
-    ron_path.push_str(".ron");
-
-    loader.load(ron_path, SpriteSheetFormat(texture_handle), (), &sprite_sheet_store)
 }
